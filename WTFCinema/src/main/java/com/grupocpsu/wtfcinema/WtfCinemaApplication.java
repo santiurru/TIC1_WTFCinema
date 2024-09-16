fc14cdfac728fceb9f805c4b81bd49a4e31fab69
Alessio Sosa <alessio.sosa.2005@gmail.com>
2024-09-16 09:04:07 -0300

    se crea el proyecto en spring boot

---
+++ WTFCinema/src/main/java/com/grupocpsu/wtfcinema/WtfCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.grupocpsu.wtfcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtfCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtfCinemaApplication.class, args);
+    }
+
+}
