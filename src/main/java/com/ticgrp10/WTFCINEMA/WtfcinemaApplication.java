b88f254cb5bcd38b7580a65f3a9708fe6d7cf3b0
Alessio Sosa <alessio.sosa.2005@gmail.com>
2024-10-28 09:31:07 -0300

    nuevo proyecto por errores

---
+++ src/main/java/com/ticgrp10/WTFCINEMA/WtfcinemaApplication.java
@@ -0,0 +1,13 @@
+package com.ticgrp10.WTFCINEMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtfcinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtfcinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticgrp10/WTFCINEMA/WtfcinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticgrp10.WTFCINEMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtfcinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
